d2e0e2aa76b9b6e436d459ecf6e0bd47b0079a86
basteazik-Man <basteazik@gmail.com>
2025-11-15 11:05:29 +0300

    цены

---
+++ src/data/prices/category-services.js
@@ -0,0 +1,11 @@
+// Автоматически сгенерировано Chip&Gadget Admin
+export const SERVICES_BY_CATEGORY = {
+  "laptops": [
+    {
+      "name": "Замена матрицы",
+      "price": "от 5000р"
+    }
+  ]
+};
+
+export const SERVICES = Object.values(SERVICES_BY_CATEGORY).flat();
